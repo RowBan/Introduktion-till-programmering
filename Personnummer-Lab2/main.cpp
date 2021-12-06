f31d07d874ab9424d11478e6a2159101f963a4de
Lipstick <robert.eriksson89@hotmail.com>
2021-12-06 01:16:35 +0100

    2021-12-06

--- Personnummer-Lab2/main.cpp
+++ Personnummer-Lab2/main.cpp
@@ -10,20 +10,42 @@ Introduktion programmering 7,5hp
 int main()
 {
 
-    std::ifstream personer;                     //Variabel för data från names.txt
-    personer.open("namestest.txt");             //Öppnad names.txt
-    if(personer.is_open())                      //Verifiera att names.txt öppnats
+    std::ifstream person;                                                                       //ifstream variable for reading from file
+    std::ofstream personNew;                                                                    //ofstream variable for output to new file
+    person.open("names.txt");                                                                   //Open names.txt for instream
+    std::string firstName, surName, nID, Adr, gender;                                           //Variable string for holding data from names.txt
+
+    if(person.is_open())                                                                        //Verify file open
     {
-        std::cout << "Filen öppnades korrekt" << std::endl;
-        /*while(personer.is_open())
+        std::cout << "File open.\n" << std::endl;
+        personNew.open("newnames.txt");                                                         //Open newnames.txt for outstream
+        
+        
+        while(!person.eof())
+        {
+            getline(person, firstName, ' ');
+            getline(person, surName);
+            getline(person, nID);
+            getline(person, Adr);
+            //if((nID[8] - '0') % 2 == 0)                                                       //Metod 1 för modulus av personnummer på index 8
+            if(static_cast<int>((nID.at(8) - 48)) % 2 == 0)                                     //Metod 2 för modulus av personnummer på index 8
             {
-                std::cout << x << std::endl;
-            }*/
-        personer.close();                       //Stäng filen, frigör resurser
-        std::cout << "Filen stängd." << std::endl;
+                gender = " [F]";
+            }
+            else
+            {
+                gender = " [M]";
+            }
+            //std::cout << static_cast<int>((nID.at(8) - 48) % 2 == 0) << std::endl;
+            std::cout << surName << ", " << firstName << gender << std::endl << Adr << std::endl;
+            personNew << surName << ", " << firstName << gender << std::endl << Adr << std::endl;
+        }
+        personNew.close();
+        person.close();                                                                         //Close file
+        std::cout << "\nFile closed." << std::endl;
     }
     else
-        std::cout << "Filen kunde inte öppnas" << std::endl;
+        std::cout << "File could not open." << std::endl;
     
 
 return(0);
